87a99046554dc554309cfd6361e4807e4bec8512
IvanKent <ivankentmalate.landicho@bicol-u.edu.ph>
2024-01-19 23:39:57 +0800

    added a loader

--- src/QuoteFetcherLoader.jsx
+++ src/QuoteFetcherLoader.jsx
@@ -3,21 +3,25 @@ import { useEffect, useState } from "react";
 
 export default function QuoteFetcher() {
     const [quote, setQuote] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
+        async function fetchAndSetQuote(){
+            const response = await fetch(RANDOM_QUOTE_URL);
+            const jsonResponse = await response.json();
+            const randomQuote = jsonResponse.quote;
+            setQuote(randomQuote);
+            setIsLoading(false)
+        }
         fetchAndSetQuote();
     
     }, [])
-    async function fetchAndSetQuote(){
-        const response = await fetch(RANDOM_QUOTE_URL);
-        const jsonResponse = await response.json();
-        const randomQuote = jsonResponse.quote;
-        setQuote(randomQuote);
-    }
+
     return (
         <div>
+            {isLoading && <p>Loading...</p>}
             <h1>{quote.text}</h1>
             <h3>{quote.author}</h3>
-            <button onClick={fetchAndSetQuote}>Get Quote Using handler</button>
         </div>
     )
 }
